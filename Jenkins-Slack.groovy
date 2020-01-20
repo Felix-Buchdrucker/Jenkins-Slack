7cb6c474fba3d29d611989cd663836175faa2830
Buchdrucker <buchdfel@deher1m1238.emea.adsint.biz>
2020-01-20 08:01:04 +0100

    renaming Jenkinsfile to groovy

---
+++ Jenkins-Slack.groovy
@@ -0,0 +1,31 @@
+// SOLUTION NO.1
+node{
+    stage("intro"){
+        echo "Hello from Jenkins"
+    }
+    stage("return date"){
+        sh 'date > outFile'
+        curDate = readFile 'outFile'
+        echo "The current date is ${curDate}"
+    }
+    stage("Slack Notification"){
+        slackSend baseUrl: 'https://hooks.slack.com/services/', channel: '#90-day-plan', color: 'danger', 
+        message: "The current date is ${curDate}", teamDomain: 'adidas', tokenCredentialId: 'Pipeline-Slack-Token', username: 'Slack-Bot'
+    }
+    
+}
+
+// SOLUTION NO.2
+// node {
+//     stage("intro"){
+//         echo "Hello from Jenkins"
+//     }
+//     stage("return date"){
+//         CurrDate = sh(script: "date", returnStdout: true)
+//         return CurrDate
+//     }
+//     stage("Slack Notification"){
+//         slackSend baseUrl: 'https://hooks.slack.com/services/', channel: '#90-day-plan', color: 'danger', 
+//         message: "The current date is ${CurrDate}", teamDomain: 'adidas', tokenCredentialId: 'Pipeline-Slack-Token', username: 'Slack-Bot'
+//     }
+// }
